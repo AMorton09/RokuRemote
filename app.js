178ca9babd1d1ea7b8b49e461acedd513b1a7e82
Andrew Morton <morton09@me.com>
2017-09-11 10:29:48 -0500

    first setup need tv to test

---
+++ app.js
@@ -0,0 +1,34 @@
+const Roku = require('rokujs');
+const express = require('express');
+const hbs = require('express-handlebars');
+const app     = express();
+
+
+//setting view engine
+app.engine('handlebars', hbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.listen('3000');
+
+console.log("im alive");
+
+app.get('/',function (req, res) {
+
+
+    Roku.discover(function (devices) {
+
+        console.log(devices);
+
+        const roku = new Roku(devices.address);
+        roku.apps(function (err, apps) {
+            if (err){
+                console.log(err);
+            }
+            console.log(apps);
+
+        });
+
+    });
+
+
+    res.render('home')
+})
